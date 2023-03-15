80e0044a5d287f1ba203fe3964bdae28a6247331
NguyenTienDat098 <datga2442004@gmail.com>
2023-03-15 21:38:40 +0700

    my-style server

--- src/configs/dbconfig.js
+++ src/configs/dbconfig.js
@@ -2,12 +2,9 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    mongoose.connect(
-      "mongodb+srv://nguyentiendat098:LfMB817PTlOLJXMh@cluster0.rv7ojc4.mongodb.net/?retryWrites=true&w=majority/Codeui",
-      {
-        useNewUrlParser: true,
-      }
-    );
+    mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+    });
     mongoose.connection.on("error", (error) => console.log(error));
     mongoose.connection.on("open", () => {
       console.log("Connected to MongoDB database.");
