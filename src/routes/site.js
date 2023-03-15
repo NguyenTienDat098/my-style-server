6c2fb0285b85d12e5645afb753ea08f2ff86bc4a
NguyenTienDat098 <datga2442004@gmail.com>
2023-03-15 17:03:56 +0700

    my-style server

--- src/routes/site.js
+++ src/routes/site.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const sitesController = require("../app/controllers/SiteController");
 
 router.get("/api/template/:id", sitesController.template);
-router.post("/find/:slug", sitesController.find);
+router.post("/api/find/:slug", sitesController.find);
 router.post("/api", sitesController.add);
 router.get("/", sitesController.index);
 router.get("/api", sitesController.data);
